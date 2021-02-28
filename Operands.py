fa18fa1682b6821a5ed1c63511347275f27fd18e
x <seybaxscorpion@gmail.com>
2021-02-28 16:52:04 +0000

    here we go

--- Operands.py
+++ Operands.py
@@ -20,3 +20,25 @@ class  UnOp():
     
     def __repr__(self):
         return f"( {self.operator.type}|{self.node} )"
+
+
+
+class  NnOp():
+
+    def __init__(self,n,v,t,o="getVal"):
+        self.name = n
+        self.value = v
+        self.type = t
+        self.operation = o
+    
+    def __repr__(self):
+        return f"( {self.operation} on {self.name} )"
+
+class VarOp(NnOp):
+    def __init__(self,n,v,t,o):
+        super().__init__(n,v,t,o)
+        
+class ConstOp(NnOp):
+    def __init__(self,n,v,t,o):
+        super().__init__(n,v,t,o)
+        
